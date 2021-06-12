bb7b46fd6eeab3bd93364d93c9a61fdcbd384a44
Kriger6 <sasa.palinkas6@gmail.com>
2021-06-13 01:05:40 +0200

    Minor update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ function Home() {
             <div className="home">
             <h1>Welcome to Phonestore</h1>
             <h3>We have the latest smartphones on the market</h3>
-            <Image />
+            <Image style={{paddingTop: "100px"}}/>
             </div>
         </div>
     )
