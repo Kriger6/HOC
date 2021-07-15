12fcd6baa984d4414c3776f23c4dd59295c38988
Kriger6 <sasa.palinkas6@gmail.com>
2021-07-15 21:44:04 +0200

    more work done

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
+
+const url = 'https://the-mobile-store.herokuapp.com/api/phones/'
 
 export default function Products() {
+    const [dataRes, setData] = useState(null)
+
+
+    useEffect(() => {
+        axios.get(url).then(response => { setData(response.data) })
+    }, [])
+    
+    
+
     return (
         <div>
             <h1>Products</h1>
+            {dataRes != null && <h2>{dataRes[0].model}</h2>}
         </div>
     )
 }
