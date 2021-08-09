f2d7df6660f3e29573ec3a5b2bc6168cfcee9f5f
Kriger6 <sasa.palinkas6@gmail.com>
2021-08-09 15:51:14 +0200

    more work done

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -4,15 +4,25 @@ import {useEffect, useState} from 'react'
 
 function Product(props) {
     const [data, setData] = useState(null);
-
+    const [endpoint, setEndpoint] = useState(null)
+    
     useEffect(() => {
-        fetch('https://the-mobile-store.herokuapp.com/api/phones/').then(response => response.json(response)).then(data => setData(data))
+        const fetchData = async () => {
+            await fetch('https://the-mobile-store.herokuapp.com/api/phones/')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .then(setEndpoint(window.location.pathname.split('/')[2]))
+        }
+
+        fetchData()
     }, [])
+
+    console.log(window.location.pathname.split('/')[2]);
     const id = props.match.params.id
     return (
         <div>
             <h1>Item</h1>
-            {data !== null ? 
+            {data !== null && data !== undefined ? 
             <>
             <h3>{data[id - 1].model}</h3> 
             <h3>{data[id - 1].price}</h3>
